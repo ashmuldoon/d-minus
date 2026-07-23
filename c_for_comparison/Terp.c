03d868c7576bba3102f32852219ac50a7eef0505
Ash <ashsmuldoon@gmail.com>
2026-07-23 23:07:07 +0100

    rerethought the language. implemented ! as the only interface with the bits. the language is still (shockingly) turing complete.

--- c_for_comparison/Terp.c
+++ c_for_comparison/Terp.c
@@ -3,7 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAPE_SIZE 30000
+#define TAPE_BITS 30000 // tape length, in bits now
+#define TAPE_BYTES ((TAPE_BITS + 7) / 8)
+
+// bit helpers: cell `p` lives in byte p/8, bit position p%8
+#define GET_BIT(tape, p) ((tape[(p) / 8] >> ((p) % 8)) & 1)
+#define FLIP_BIT(tape, p) (tape[(p) / 8] ^= (1 << ((p) % 8)))
+#define SET_BIT(tape, p, v) \
+    (v ? (tape[(p) / 8] |= (1 << ((p) % 8))) : (tape[(p) / 8] &= ~(1 << ((p) % 8))))
 
 int main(int argc, char **argv)
 {
@@ -18,51 +25,64 @@ int main(int argc, char **argv)
     fclose(f);
 
     int *match = malloc(len * sizeof(int));
-    int stack[len], sp = 0;
-
-    for (long i = 0; i < len; i++) // this is why I'm not terribly pressed over the non null-terminated string we're storing
-    {                              // this could never go past len, that said, we should absolutely never use any string method or whatever
-        if (code[i] == '[')
+    int *stack = malloc(len * sizeof(int));
+    int sp = 0;
+    for (long i = 0; i < len; i++)
+    {                       // this is why I'm not terribly pressed over the non null-terminated string we're storing
+        if (code[i] == '[') // this could never go past len, that said, we should absolutely never use any string method or whatever
             stack[sp++] = i;
         else if (code[i] == ']')
         {
-            int open = stack[--sp]; // recursion check
+            int open = stack[--sp];
             match[open] = i;
             match[i] = open;
         }
     }
+    free(stack);
 
-    unsigned char tape[TAPE_SIZE] = {0};
-    int ptr = 0;
+    unsigned char tape[TAPE_BYTES] = {0}; // bit-addressed tape
+    int ptr = 0;                          // now a BIT index, not byte index
+
+    // defining terms (i dont think i could really improve on this in terms of memory usage, right?)
 
     for (long pc = 0; pc < len; pc++)
     {
         switch (code[pc])
         {
-        case '>': // defining terms (i dont think i could really improve on this in terms of memory usage, right?)
+        case '>':
             ptr++;
             break;
         case '<':
             ptr--;
             break;
-        case '+':
-            tape[ptr]++;
-            break;
-        case '-':
-            tape[ptr]--;
-            break;
+        case '!':
+            FLIP_BIT(tape, ptr);
+            break; // replaces + and -
         case '.':
-            putchar(tape[ptr]);
+        {
+            // pack bits [ptr .. ptr+7] into a byte, ptr = MSB
+            unsigned char out = 0;
+            for (int b = 0; b < 8; b++)
+                out |= GET_BIT(tape, ptr + b) << (7 - b);
+            putchar(out);
             break;
+        }
         case ',':
-            tape[ptr] = getchar();
+        {
+            // unpack one input byte into bits [ptr .. ptr+7], ptr = MSB
+            int in = getchar();
+            if (in == EOF)
+                in = 0; // decide EOF behavior explicitly
+            for (int b = 0; b < 8; b++)
+                SET_BIT(tape, ptr + b, (in >> (7 - b)) & 1);
             break;
+        }
         case '[':
-            if (!tape[ptr])
+            if (!GET_BIT(tape, ptr))
                 pc = match[pc];
             break;
         case ']':
-            if (tape[ptr])
+            if (GET_BIT(tape, ptr))
                 pc = match[pc];
             break;
         }
